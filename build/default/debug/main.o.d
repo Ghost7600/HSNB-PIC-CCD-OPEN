f87064e958fcf6378ba33319a79fa87c1725d837
Ghost7600 <lg20093@WIN.fh-nb.de>
2022-10-17 15:19:06 +0200

    machine repo, should compile

---
+++ build/default/debug/main.o.d
@@ -0,0 +1,7 @@
+build/default/debug/main.o: main.c config.h I2Cdrv.h process_sample.h
+
+config.h:
+
+I2Cdrv.h:
+
+process_sample.h:
---
+++ build/default/production/main.o.d
@@ -0,0 +1,8 @@
+build/default/production/main.o: main.c config.h I2Cdrv.h \
+ process_sample.h
+
+config.h:
+
+I2Cdrv.h:
+
+process_sample.h:
