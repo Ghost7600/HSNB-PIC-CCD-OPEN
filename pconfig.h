6c11bb1cc447f8c15c421f550b9dc869a445fbb9
Ghost7600 <lg20093@WIN.fh-nb.de>
2022-10-19 15:29:25 +0200

    moved ports, ADC and OSC configs to pconfig files

---
+++ pconfig.h
@@ -0,0 +1,55 @@
+/* 
+ * File: pconfig.h  
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef XC_PCONFIG_H
+#define	XC_PCONFIG_H
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+
+// TODO Insert appropriate #include <>
+
+// TODO Insert C++ class definitions if appropriate
+
+// TODO Insert declarations
+
+// Comment a function and leverage automatic documentation with slash star star
+
+
+    void pconfig (void);
+/**
+  
+    <p><b>Summary: do initial setup of ports and peripherals</b></p>
+
+    <p><b>Parameters: null </b></p>
+
+    <p><b>Returns: null </b></p>
+
+    <p><b>Example:</b></p>
+    <code>
+    void pconfig (void);
+    </code>
+
+    <p><b>Remarks:</b></p>
+ */
+// TODO Insert declarations or function prototypes (right here) to leverage 
+// live documentation
+
+#ifdef	__cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+    // TODO If C++ is being used, regular C code needs function names to have C 
+    // linkage so the functions can be used by the c code. 
+
+#ifdef	__cplusplus
+}
+#endif /* __cplusplus */
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
