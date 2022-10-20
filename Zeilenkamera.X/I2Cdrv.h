fa25efa22140dbac5f349d8d6958d12c091a54f5
kenner.trabalhos@gmail.com <lg20093@WIN.fh-nb.de>
2022-10-20 15:17:42 +0200

    added files

---
+++ Zeilenkamera.X/I2Cdrv.h
@@ -0,0 +1,26 @@
+/* 
+ * File:   I2Cdrv.h
+ * Author: hahn
+ *
+ * Created on 16. September 2022, 09:48
+ */
+
+#ifndef I2CDRV_H
+#define	I2CDRV_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+void transfer_info(int *i2c_buffer)
+{
+    
+}
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* I2CDRV_H */
+
